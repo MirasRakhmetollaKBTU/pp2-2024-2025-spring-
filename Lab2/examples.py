9246779de465d9ac0ec2a4a951290128710e7619
Rakhmetolla Miras Askhatuly <r.miras.work@gmail.com>
2025-01-22 11:31:16 +0500

    Operations

--- Lab2/examples.py
+++ Lab2/examples.py
@@ -65,3 +65,17 @@ else:
 # Example 10
 x = 200
 print(isinstance(x, int))
+
+### Operators
+
+# Example 11
+print(10 + 5)
+
+# Example 12
+print((6 + 3) - (6 + 3))
+
+# Example 13
+print(100 + 5 * 3)
+
+# Example 14
+print(5 + 4 - 7 + 3)
